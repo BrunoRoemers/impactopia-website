33762a207614b42b556865e921c0e928bce875bd
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-08-21 21:12:02 +0200

    Improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,24 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h1 className="title is-size-3 has-text-weight-bold is-bold-light">
+                404 - Page Not Found
+              </h1>
+              <p>Unfortunately this page doesn't exist anymore.</p>
+              <Link to="/">Go back to the homepage.</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
