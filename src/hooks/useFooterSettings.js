a81483bc4e1db4ede4d3b4591836750fe530b509
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-08-21 22:43:44 +0200

    Configure footer logo

--- src/hooks/useFooterSettings.js
+++ src/hooks/useFooterSettings.js
@@ -1,15 +1,15 @@
 import { graphql, useStaticQuery } from 'gatsby'
 
 const useFooterSettings = () => {
-  const { headerSettings: footerSettings } = useStaticQuery(
+  const { footerSettings } = useStaticQuery(
     graphql`
       query FOOTER_SETTINGS_QUERY {
         footerSettings {
-          temp
+          logoImage
         }
       }
     `
-  )
+  );
   return footerSettings
 }
 
